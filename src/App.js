d870c92879dbb3a2812f95151df9868fda5564a3
nelo <nelokwaz@gmail.com>
2020-11-02 20:43:12 -1200

    structured the jumbotron component and added some styles using styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React from "react";
+import Jumbotron from "./components/jumbotron";
+import JumboData from "./fixtures/jumbo";
 
 export default function App() {
-  return <p>Hello</p>;
+  return (
+    <Jumbotron.Container>
+      {JumboData.map((data) => (
+        <Jumbotron key={data.id} direction={data.direction}>
+          <p>Hello</p>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
+  );
 }
---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container, Inner } from "./styles/jumbotron";
+
+export default function Jumbotron({
+  children,
+  direction = "row",
+  ...restProps
+}) {
+  return (
+    <Inner direction={direction}>
+      <p>Hello Again</p>
+    </Inner>
+  );
+}
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};
