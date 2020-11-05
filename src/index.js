31fc2f1bf76b00a7122b635b1660bb710beb75c6
nelo <nelokwaz@gmail.com>
2020-11-04 16:18:14 -1200

    created a global style for the html and body element

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./app";
 import reportWebVitals from "./reportWebVitals";
-import GlobalStyles from "./global-styles";
+import { GlobalStyles } from "./global-styles";
 
-render(<><GlobalStyles/><App /></>, document.getElementById("root"));
+render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
