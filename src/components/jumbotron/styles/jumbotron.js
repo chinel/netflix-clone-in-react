0cf37f951e786b5c75491a85d4127a6262e2e35b
nelo <nelokwaz@gmail.com>
2020-11-04 15:50:02 -1200

    styled the the jumbotron elements

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -16,11 +16,33 @@ export const Inner = styled.div`
 
 export const Pane = styled.div`
   width: 50%;
+
+  @media (max-width: 1000px) {
+    width: 100%;
+    margin: 0 45px;
+    text-align: center;
+  }
 `;
 
-export const Title = styled.h1``;
+export const Title = styled.h1`
+  font-size: 50px;
+  line-height: 1.1;
+  margin-bottom: 8px;
 
-export const SubTitle = styled.h2``;
+  @media (max-width: 600px) {
+    font-size: 35px;
+  }
+`;
+
+export const SubTitle = styled.h2`
+  font-size: 26px;
+  font-weight: normal;
+  line-height: normal;
+
+  @media (max-width: 600px) {
+    font-size: 16px;
+  }
+`;
 
 export const Image = styled.img`
   width: 100%;
