22b36c17172c22773c7c351399cb72934029edc5
nelo <nelokwaz@gmail.com>
2020-11-05 20:11:04 -1200

    created an index.js file as a single source of truth for all components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import React from "react";
-import Jumbotron from "./components/jumbotron";
-import JumboData from "./fixtures/jumbo";
+import { JumbotronContainer } from "./containers/jumbotron";
 
 export default function App() {
-  return (
-    <Jumbotron.Container>
-      {JumboData.map((data) => (
-        <Jumbotron key={data.id} direction={data.direction}>
-          <Jumbotron.Pane>
-            <Jumbotron.Title>{data.title}</Jumbotron.Title>
-            <Jumbotron.SubTitle>{data.subTitle}</Jumbotron.SubTitle>
-          </Jumbotron.Pane>
-          <Jumbotron.Pane>
-            <Jumbotron.Image src={data.image} alt={data.alt} />
-          </Jumbotron.Pane>
-        </Jumbotron>
-      ))}
-    </Jumbotron.Container>
-  );
+  return <JumbotronContainer />;
 }
