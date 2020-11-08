dba342b7501b22867acb94c4aefc27e60dbc611d
nelo <nelokwaz@gmail.com>
2020-11-08 06:04:55 +0100

    made some adjustments to the footer style

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -20,10 +20,10 @@ export const Column = styled.div`
 
 export const Row = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(210px, 1fr));
   grid-gap: 15px;
 
-  @media (max-width: 1000px) {
+  @media (max-width: 959px) {
     grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
   }
 `;
@@ -31,7 +31,8 @@ export const Row = styled.div`
 export const Link = styled.a`
   color: #757575;
   margin-bottom: 20px;
-  font-size: 13px;
+  font-size: 14px;
+  text-decoration: none;
 `;
 
 export const Title = styled.p`
@@ -48,5 +49,5 @@ export const Text = styled.p`
 
 export const Break = styled.p`
   flex-basis: 100%;
-  height: 0; 
+  height: 0;
 `;
