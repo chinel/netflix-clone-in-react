3dd32d03f1998348c8c6003cc8c53242eb8edf08
nelo <nelokwaz@gmail.com>
2020-11-08 05:29:39 +0100

    added the footer conatiner to the app component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
+import FooterContainer from "./containers/footer";
 import { JumbotronContainer } from "./containers/jumbotron";
 
 export default function App() {
-  return <JumbotronContainer />;
+  return <><JumbotronContainer /><FooterContainer/></>;
 }
