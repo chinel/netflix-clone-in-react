e488afe71d168939719370b21303121649542d35
nelo <nelokwaz@gmail.com>
2020-11-11 05:09:30 +0100

    created the faqs container

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,3 +1,17 @@
 import React from "react";
 import { Accordion } from "../components";
 import faqsData from "../fixtures/faqs";
+
+export function AccordionContainer() {
+  return (
+    <Accordion>
+      <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+      {faqsData.map((item) => (
+        <Accordion.Item key={item}>
+          <Accordion.Header>{item.header}</Accordion.Header>
+          <Accordion.Body>{item.body}</Accordion.Body>
+        </Accordion.Item>
+      ))}
+    </Accordion>
+  );
+}
