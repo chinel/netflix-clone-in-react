4a38c911ebdffb06ef52ebfe32692cefbb1f3423
nelo <nelokwaz@gmail.com>
2020-11-11 08:27:50 +0100

    styled some of the accordion components

--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -1,15 +1,47 @@
 import styled from "styled-components/macro";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  border-bottom: 8px solid #222;
+`;
 
-export const Item = styled.div``;
+export const Inner = styled.div`
+  display: flex;
+  padding: 70px 45px;
+  flex-direction: column;
+  max-width: 815px;
+  margin: auto;
+`;
 
-export const Header = styled.div``;
+export const Item = styled.div`
+  color: #fff;
+  margin-bottom: 10px;
+
+  &:first-of-type {
+    margin-top: 3em;
+  }
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
 export const Body = styled.div``;
 
-export const Frame = styled.div``;
+export const Frame = styled.div`
+  margin-bottom: 40px;
+`;
 
-export const Inner = styled.div``;
+export const Title = styled.h1`
+  font-size: 50px;
+  line-height: 1.1;
+  margin-top: 0;
+  margin-bottom: 8px;
+  color: #fff;
+  text-align: center;
 
-export const Title = styled.h1``;
+  @media (max-width: 600px) {
+    font-size: 35px;
+  }
+`;
