ded8dd5a8bfccb5111bfb70198c21686708f69c8
nelo <nelokwaz@gmail.com>
2020-11-12 05:21:27 +0100

    styled some of the components of the opt form component

--- src/components/opt-form/styles/opt-form.js
+++ src/components/opt-form/styles/opt-form.js
@@ -1,8 +1,26 @@
 import styled from "styled-components/macro";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  margin-top: 20px;
+  flex-wrap: wrap;
 
-export const Input = styled.input``;
+  @media (max-width: 1000px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const Input = styled.input`
+  max-width: 450px;
+  width: 100%;
+  border: 0;
+  padding: 10px;
+  height: 70px;
+  box-sizing: border-box;
+`;
 
 export const Button = styled.button``;
 
