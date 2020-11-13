23f77bf3f0ea43dec92362ef9a9016ca034f4cae
nelo <nelokwaz@gmail.com>
2020-11-13 05:32:37 +0100

    completed the accordion styled components

--- src/components/opt-form/styles/opt-form.js
+++ src/components/opt-form/styles/opt-form.js
@@ -22,6 +22,52 @@ export const Input = styled.input`
   box-sizing: border-box;
 `;
 
-export const Button = styled.button``;
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  height: 72px;
+  background: #e50914;
+  color: #fff;
+  text-transform: uppercase;
+  padding: 0 32px;
+  font-size: 26px;
+  border: 0;
+  cursor: pointer;
 
-export const Text = styled.p``;
+  &hover: {
+    background: #f40612;
+  }
+
+  @media (max-width: 1000px) {
+    height: 50px;
+    font-size: 16px;
+    margin-top: 20px;
+    font-weight: bold;
+  }
+
+  img {
+    margin-left: 10px;
+    filter: brightness(0) invert(1);
+    width: 24px;
+
+    @media (max-width: 1000px) {
+      width: 16px;
+    }
+  }
+`;
+
+export const Text = styled.p`
+  font-size: 19.2px;
+  color: #fff;
+  text-align: center;
+
+  @media (max-width: 600px) {
+    font-size: 16px;
+    line-height: 22px;
+  }
+`;
+
+export const Break = styled.div`
+  flex-basis: 100%;
+  height: 0;
+`;
