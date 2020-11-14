ee1a0e0b765301d430958de30b5c05034ce21c69
nelo <nelokwaz@gmail.com>
2020-11-14 07:04:38 +0100

    create a single source of truth for all pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
-import Home from "./pages/home";
+import { Home } from "./pages";
 
 export default function App() {
   return (
