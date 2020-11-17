2aa641169acef9eea593dad92209f61ff79540d0
nelo <nelokwaz@gmail.com>
2020-11-17 09:39:36 +0100

    completed the accordion for the feature component

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,15 +3,19 @@ import FooterContainer from "../containers/footer";
 import FaqContainer from "../containers/faqs";
 import JumbotronContainer from "../containers/jumbotron";
 import HeaderContainer from "../containers/header";
-import { OptForm } from "../components";
+import { Feature, OptForm } from "../components";
 
 export default function Home() {
   return (
     <>
       <HeaderContainer>
         <Feature>
-          <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
-          <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle> 
+          <Feature.Title>
+            Unlimited films, TV programmes and more.
+          </Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at any time.
+          </Feature.SubTitle>
         </Feature>
         <OptForm>
           <OptForm.Input placeholder="Email Address" />
