7a064545adc8ee1170d811452e92663041c51db0
nelo <nelokwaz@gmail.com>
2020-11-17 07:47:59 +0100

    added the optForm reusable component to the header

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,13 +3,25 @@ import FooterContainer from "../containers/footer";
 import FaqContainer from "../containers/faqs";
 import JumbotronContainer from "../containers/jumbotron";
 import HeaderContainer from "../containers/header";
+import { OptForm } from "../components";
 
 export default function Home() {
   return (
-    <HeaderContainer>
+    <>
+      <HeaderContainer>
+        <OptForm>
+          <OptForm.Input placeholder="Email Address" />
+          <OptForm.Button>Try it now</OptForm.Button>
+          <OptForm.Break />
+          <OptForm.Text>
+            Ready to watch? Enter your email address to create or restart
+            membership
+          </OptForm.Text>
+        </OptForm>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqContainer />
       <FooterContainer />
-    </HeaderContainer>
+    </>
   );
 }
