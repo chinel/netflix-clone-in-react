599e0f23b260bd58892f24d1e888bd96c6a023dc
nelo <nelokwaz@gmail.com>
2020-11-17 09:40:29 +0100

    moved the opt form inside of the feature component

--- src/pages/home.js
+++ src/pages/home.js
@@ -16,16 +16,16 @@ export default function Home() {
           <Feature.SubTitle>
             Watch anywhere. Cancel at any time.
           </Feature.SubTitle>
+          <OptForm>
+            <OptForm.Input placeholder="Email Address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to watch? Enter your email address to create or restart
+              membership
+            </OptForm.Text>
+          </OptForm>
         </Feature>
-        <OptForm>
-          <OptForm.Input placeholder="Email Address" />
-          <OptForm.Button>Try it now</OptForm.Button>
-          <OptForm.Break />
-          <OptForm.Text>
-            Ready to watch? Enter your email address to create or restart
-            membership
-          </OptForm.Text>
-        </OptForm>
       </HeaderContainer>
       <JumbotronContainer />
       <FaqContainer />
