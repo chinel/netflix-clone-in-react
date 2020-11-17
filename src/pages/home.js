4b243a9a0900bbc5aed258d134cbb1eadc979ae6
nelo <nelokwaz@gmail.com>
2020-11-17 07:54:56 +0100

    added the feature section to the header component

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,6 +9,10 @@ export default function Home() {
   return (
     <>
       <HeaderContainer>
+        <Feature>
+          <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+          <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle> 
+        </Feature>
         <OptForm>
           <OptForm.Input placeholder="Email Address" />
           <OptForm.Button>Try it now</OptForm.Button>
