8946886823249ef9fe99188472eb3a07dd3d5177
nelo <nelokwaz@gmail.com>
2020-11-19 08:47:09 +0100

    completed the form components using compound component pattern

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -15,10 +15,34 @@ export default function Form({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
 }
 
+Form.Base = function FormBase({ children, ...restProps }) {
+  return <Base {...restProps}>{children}</Base>;
+};
+
 Form.Error = function FormError({ children, ...restProps }) {
   return <Error {...restProps}>{children}</Error>;
 };
 
-Form.Base = function FormBase({ children, ...restProps }) {
-  return <Base {...restProps}>{children}</Base>;
+Form.Title = function FormTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Form.Text = function FormText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Form.TextSmall = function FormTextSmall({ children, ...restProps }) {
+  return <TextSmall {...restProps}>{children}</TextSmall>;
+};
+
+Form.TextLink = function FormLink({ children, ...restProps }) {
+  return <Link {...restProps}>{children}</Link>;
+};
+
+Form.Input = function FormInput({ children, ...restProps }) {
+  return <Input {...restProps}>{children}</Input>;
+};
+
+Form.Submit = function FormSubmit({ children, ...restProps }) {
+  return <Submit {...restProps}>{children}</Submit>;
 };
