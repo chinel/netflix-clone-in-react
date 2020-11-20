fff088c839ba63990cc1ca01e296c4d90b81ae93
nelo <nelokwaz@gmail.com>
2020-11-20 08:45:47 +0100

    started setting up the sign in form

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { FooterContainer, HeaderContainer } from "../containers";
+import { Form } from "../components";
 
 export default function Signin() {
+  const [emailAddress, setEmailAddress] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState();
+
+  //check form elements are valid
+  //Email and password
   return (
     <>
-      <HeaderContainer>Hello From Signin</HeaderContainer>
+      <HeaderContainer>
+        <Form></Form>
+      </HeaderContainer>
       <FooterContainer />
     </>
   );
