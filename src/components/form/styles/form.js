9b269661a40b480b1d26f762d82f1a0570d37a1e
nelo <nelokwaz@gmail.com>
2020-11-21 19:07:58 -1200

    added some styles to the form style component

--- src/components/form/styles/form.js
+++ src/components/form/styles/form.js
@@ -1,17 +1,55 @@
 import styled from "styled-components/macro";
 import { Link as ReactRouterLink } from "react-router-dom";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 660px;
+  background-color: rgba(0, 0, 0, 0.75);
+  border-radius: 5px;
+  box-sizing: border-box;
+  width: 100%;
+  margin: auto;
+  max-width: 450px;
+  padding: 60px 68px 40px;
+  margin-bottom: 100px;
+`;
 
-export const Error = styled.div``;
+export const Error = styled.div`
+  background: #e87c03;
+  border-radius: 4px;
+  font-size: 14px;
+  margin: 0 0 16px;
+  color: white;
+  padding: 15px 20px;
+`;
 
-export const Base = styled.form``;
+export const Base = styled.form`
+  display: flex;
+  flex-direction: column;
+  max-width: 450px;
+  width: 100%;
+`;
 
-export const Title = styled.h1``;
+export const Title = styled.h1`
+  color: #fff;
+  font-size: 32px;
+  font-weight: bold;
+  margin-bottom: 28px;
+`;
 
-export const Text = styled.p``;
+export const Text = styled.p`
+  color: #737373;
+  font-size: 16px;
+  font-weight: 500;
+`;
 
-export const TextSmall = styled.p``;
+export const TextSmall = styled.p`
+  margin-top: 10px;
+  font-size: 13px;
+  line-height: normal;
+  color: #8c8c8c;
+`;
 
 export const Link = styled(ReactRouterLink)`
   color: white;
