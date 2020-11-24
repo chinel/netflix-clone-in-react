9314953dcedfa8f8b4871cfd5bebfa9bf77e9d7a
nelo <nelokwaz@gmail.com>
2020-11-24 06:36:49 +0100

    structured the sign up page

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,5 +1,64 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { FirebaseContext } from "../context/firebase";
+import { useHistory } from "react-router-dom";
+import { FooterContainer, HeaderContainer } from "../containers";
+import { Form } from "../components";
+import * as ROUTES from "../constants/routes";
 
 export default function Signup() {
-  return <p>Hello From Signup</p>;
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+  const [firstName, setFirstName] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const isInvalid = firstName === "" || emailAddress === "" || password === "";
+
+  const handleSignUp = (event) => {
+    event.preventDefault();
+
+    //sign up with firebase
+  };
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign up</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignUp} method="POST">
+            <Form.Input
+              placeholder="First Name"
+              value={firstName}
+              onChange={({ target }) => setFirstName(target.value)}
+            />
+            <Form.Input
+              placeholder="Email Address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit type="submit" disabled={isInvalid}>
+              Sign up
+            </Form.Submit>
+          </Form.Base>
+          <Form.Text>
+            Already a user?{" "}
+            <Form.TextLink to={ROUTES.SIGN_IN}>Sign in now</Form.TextLink>
+          </Form.Text>
+          <Form.TextSmall>
+          This page is protected by Google reCaptcha to ensure that you're not
+            a bot. Learn more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
