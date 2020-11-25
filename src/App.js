227ab97197e96107583f475310fa44c65a604439
nelo <nelokwaz@gmail.com>
2020-11-25 05:26:58 +0100

    created a function to protect the browse path from unautorized users

--- src/App.js
+++ src/App.js
@@ -5,17 +5,33 @@ import { Home, Browse, Signup, Signin } from "./pages";
 import { IsUserRedirect } from "./helpers/routes";
 
 export default function App() {
+  const user = {};
+
   return (
     <Router>
+      <Route exact path={ROUTES.SIGN_IN}>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_IN}
+        >
+          <Signin />
+        </IsUserRedirect>
+      </Route>
+      <Route>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_UP}
+        >
+          <Signup />
+        </IsUserRedirect>
+      </Route>
+
       <Route exact path={ROUTES.BROWSE}>
         <Browse />
       </Route>
-      <Route exact path={ROUTES.SIGN_IN}>
-        <Signin />
-      </Route>
-      <Route exact path={ROUTES.SIGN_UP}>
-        <Signup />
-      </Route>
+
       <Route exact path={ROUTES.HOME}>
         <Home />
       </Route>
--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -1,27 +1,53 @@
 import React from "react";
-import {Route, Redirect} from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
+import * as ROUTES from "../constants/routes";
 
-export function IsUserRedirect ({user, loggedInPath, children, ...restProps}) {
-    return (
-        <Route 
-        {...restProps}
-         render={() => {
-             if(!user){
-                 return children;
-             }
-
-             if(user){
-                 return <Redirect
-                 to={{
-                     pathname: loggedInPath
-                 }}
-                 />
-             }
-            return null;
-            }
-        
+export function IsUserRedirect({ user, loggedInPath, children, ...restProps }) {
+  return (
+    <Route
+      {...restProps}
+      render={() => {
+        if (!user) {
+          return children;
         }
-        />
-    )
+
+        if (user) {
+          return (
+            <Redirect
+              to={{
+                pathname: loggedInPath,
+              }}
+            />
+          );
+        }
+        return null;
+      }}
+    />
+  );
 }
 
+export function ProtectedRoute({ user, children, ...restProps }) {
+  return (
+    <Route
+      {...restProps}
+      render={({ location }) => {
+        if (user) {
+          return children;
+        }
+
+        if (!user) {
+          return (
+            <Redirect
+              to={{
+                pathname: ROUTES.SIGN_IN,
+                state: { from: location },
+              }}
+            />
+          );
+        }
+
+        return null;
+      }}
+    />
+  );
+}
