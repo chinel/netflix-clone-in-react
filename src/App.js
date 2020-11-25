2f0bef2ba67f6e2a4d15d5349e2f4b0f4af3a790
nelo <nelokwaz@gmail.com>
2020-11-25 05:36:34 +0100

    added the newly created function to protect the browser router from unauthorized users

--- src/App.js
+++ src/App.js
@@ -2,35 +2,34 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import { Home, Browse, Signup, Signin } from "./pages";
-import { IsUserRedirect } from "./helpers/routes";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
 
 export default function App() {
   const user = {};
 
   return (
     <Router>
-      <Route exact path={ROUTES.SIGN_IN}>
-        <IsUserRedirect
-          user={user}
-          loggedInPath={ROUTES.BROWSE}
-          path={ROUTES.SIGN_IN}
-        >
-          <Signin />
-        </IsUserRedirect>
-      </Route>
-      <Route>
-        <IsUserRedirect
-          user={user}
-          loggedInPath={ROUTES.BROWSE}
-          path={ROUTES.SIGN_UP}
-        >
-          <Signup />
-        </IsUserRedirect>
-      </Route>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_IN}
+        exact
+      >
+        <Signin />
+      </IsUserRedirect>
 
-      <Route exact path={ROUTES.BROWSE}>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_UP}
+        exact
+      >
+        <Signup />
+      </IsUserRedirect>
+
+      <ProtectedRoute user={user} exact path={ROUTES.BROWSE}>
         <Browse />
-      </Route>
+      </ProtectedRoute>
 
       <Route exact path={ROUTES.HOME}>
         <Home />
