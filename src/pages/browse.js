4802d0e1de44bc6c2f808898ee1bdbf8c809dcb0
nelo <nelokwaz@gmail.com>
2020-11-27 10:58:48 +0100

    pulled out the sorted movie data to the browse page

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { useContent } from "../hooks";
+import selectionFilter from "../utils/selection-filter";
 
 export default function Browse() {
   //Retrieve series and the Films
   const { series } = useContent("series");
   const { films } = useContent("films");
-  console.log(series);
-  console.log(films);
+  //slides for the browse page
+  const slides = selectionFilter({ series, films });
   return <p>Hello From Browse</p>;
 }
