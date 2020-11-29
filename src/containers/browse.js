0683eb9892f904e8af33e03e29e298d4cc2a1d8f
nelo <nelokwaz@gmail.com>
2020-11-29 07:45:55 +0100

    pulled out the current auth user from firebase into the browser page

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import SelectProfileContainer from "./profile";
+import { FirebaseContext } from "../context/firebase";
 
 export default function BrowseContainer({ slides }) {
-  return <SelectProfileContainer />;
+  const { firebase } = useContext(FirebaseContext);
+  const user = firebase.auth().currentUser || {};
+  return <SelectProfileContainer user={user} />;
 }
