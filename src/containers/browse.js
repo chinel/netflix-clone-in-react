9ff546b142cbbbd762b74ebbf9bd4e8618b24a9b
nelo <nelokwaz@gmail.com>
2020-11-28 17:44:47 -1200

    added the profile container to the browse container

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,5 +1,6 @@
 import React from "react";
+import { SelectProfileContainer } from "./profile";
 
 export default function BrowseContainer({ slides }) {
-  return <p>Hello!</p>;
+  return <SelectProfileContainer />;
 }
