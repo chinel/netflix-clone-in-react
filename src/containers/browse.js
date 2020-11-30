cf41993f900f3fab056b50364917f4e7209742c3
nelo <nelokwaz@gmail.com>
2020-11-30 05:31:09 +0100

    added a loading state until user profile is available

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,9 +1,17 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import SelectProfileContainer from "./profile";
 import { FirebaseContext } from "../context/firebase";
 
 export default function BrowseContainer({ slides }) {
+  const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
   const { firebase } = useContext(FirebaseContext);
   const user = firebase.auth().currentUser || {};
-  return <SelectProfileContainer user={user} />;
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile.displayName]);
+  return <SelectProfileContainer user={user} setProfile={setProfile} />;
 }
