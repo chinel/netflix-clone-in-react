fc5ed068499f652e7a03c71f70bf5aa8cee5b101
nelo <nelokwaz@gmail.com>
2020-12-04 19:59:55 +0100

    styled the search input using styled component

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -182,7 +182,16 @@ export const SearchIcon = styled.button`
 `;
 
 export const SearchInput = styled.input`
-
+  background-color: #44444459;
+  color: white;
+  border: 1px solid white;
+  transition: width 0.5s;
+  height: 30px;
+  font-size: 14px;
+  margin-left: ${({ active }) => (active === true ? "10px" : "0")};
+  padding: ${({ active }) => (active === true ? "0 10px" : "10px")};
+  opacity: ${({ active }) => (active === true ? "1" : "0")};
+  width: ${({ active }) => (active === true ? "200px" : "0")};
 `;
 
 export const Profile = styled.div`
