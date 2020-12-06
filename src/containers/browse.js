9b2fb20f89e38b7bd005538963dd32267b5698c8
nelo <nelokwaz@gmail.com>
2020-12-06 15:04:05 +0100

    added the card feature layout structure

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -97,6 +97,12 @@ export default function BrowseContainer({ slides }) {
                 </Card.Item>
               ))}
             </Card.Entities>
+            {/*          <Card.Feature category={category}>
+              <Player>
+                <Player.Button />
+                <Player.Video src="/videos/bunny.mp4" />
+              </Player>
+            </Card.Feature> */}
           </Card>
         ))}
       </Card.Group>
