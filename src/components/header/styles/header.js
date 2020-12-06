33ec8fc66319ac5eafcfc3e96c82e4b4c17af6c0
nelo <nelokwaz@gmail.com>
2020-12-05 18:14:06 -1200

    added the style for the play button using styled component

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -181,6 +181,26 @@ export const SearchIcon = styled.button`
   }
 `;
 
+export const PlayButton = styled.button`
+box-shadow: 0 0.6vw 1vw -0.4vw rgba(0,0,0, 0.35);
+background-color: #e6e6e6;
+color: #000;
+border-width: 0;
+padding: 10px 20px;
+border-right: 5px;
+max-width: 130px;
+font-size: 20px;
+margin-top: 10px;
+cursor: pointer;
+transition: background-color: 0.5 ease;
+
+&:hover{
+   background-color: #ff1e1e;
+   color: #fff;
+}
+
+`
+
 export const SearchInput = styled.input`
   background-color: #44444459;
   color: white;
