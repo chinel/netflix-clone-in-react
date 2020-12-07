8874a37a012f60b5e9a77b164967ea450ad01527
nelo <nelokwaz@gmail.com>
2020-12-07 18:50:47 +0100

    corrected the background image issue on the card feature component

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -65,7 +65,7 @@ Card.Feature = function CardFeature({ children, category, ...restProps }) {
   return showFeature ? (
     <Feature
       {...restProps}
-      src={`/images/${category}/${itemFeature.genre}/${itemFeature.slug}/large.jpg}`}
+      src={`/images/${category}/${itemFeature.genre}/${itemFeature.slug}/large.jpg`}
     >
       <Content>
         <FeatureTitle>{itemFeature.title}</FeatureTitle>
@@ -73,18 +73,17 @@ Card.Feature = function CardFeature({ children, category, ...restProps }) {
         <FeatureClose onClick={() => setShowFeature(false)}>
           <img src="/images/icons/close.png" alt="Close" />
         </FeatureClose>
-      
 
-      <Group margin="30px 0" flexDirection="row" alignItems="center">
-        <Maturity rating={itemFeature.maturity}>
-          {itemFeature.maturity < 12 ? "PG" : itemFeature.maturity}
-        </Maturity>
-        <FeatureText fontWeight="bold">
-          {itemFeature.genre.charAt(0).toUpperCase() +
-            itemFeature.genre.slice(1)}
-        </FeatureText>
-      </Group>
-      {children}
+        <Group margin="30px 0" flexDirection="row" alignItems="center">
+          <Maturity rating={itemFeature.maturity}>
+            {itemFeature.maturity < 12 ? "PG" : itemFeature.maturity}
+          </Maturity>
+          <FeatureText fontWeight="bold">
+            {itemFeature.genre.charAt(0).toUpperCase() +
+              itemFeature.genre.slice(1)}
+          </FeatureText>
+        </Group>
+        {children}
       </Content>
     </Feature>
   ) : null;
