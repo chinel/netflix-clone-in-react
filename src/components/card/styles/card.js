efcd17868bb9419598645a647dd022da09548dfd
nelo <nelokwaz@gmail.com>
2020-12-07 05:13:35 +0100

    styled some of the card components using styled component

--- src/components/card/styles/card.js
+++ src/components/card/styles/card.js
@@ -1,10 +1,44 @@
 import styled from "styled-components/macro";
 
-export const Container = styled.div``;
+export const Title = styled.p`
+font-size: 24px;
+color: #e5e5e5;
+font-weight: bold;
+margin-left: 56px;
+margin-right: 56px;
+margin-top:0;
+`;
 
-export const Group = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 50px;
+  box-sizing: border-box;
 
-export const Title = styled.p``;
+  > ${Title} {
+    @media (max-width: 1000px) {
+      margin-left: 30px;
+    }
+  }
+
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+`;
+
+export const Group = styled.div`
+  display: flex;
+  flex-direction: ${({ flexDirection }) =>
+    flexDirection === "row" ? "row" : "column"};
+  ${({ alignItems }) => alignItems && `align-items: ${alignItems}`}
+  ${({ margin }) => margin && `margin: ${margin}`}
+
+  > ${Container}:first-of-type {
+    @media (min-width: 1100px) {
+      margin-top: -150px;
+    }
+  }
+`;
 
 export const SubTitle = styled.p``;
 
