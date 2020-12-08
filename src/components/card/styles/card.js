ed740c64806ce36478ab5a58ff6ce24974e245be
nelo <nelokwaz@gmail.com>
2020-12-08 18:38:16 +0100

    corrected the transitioning of the cards

--- src/components/card/styles/card.js
+++ src/components/card/styles/card.js
@@ -161,38 +161,35 @@ export const Item = styled.div`
   margin-right: 5px;
   position: relative;
   cursor: pointer;
-  transition: transform: 0.2s;
+  transition: transform 0.2s;
 
-  &:hover{
+  &:hover {
     transform: scale(1.3);
     z-index: 99;
   }
 
-  @media(min-width: 1200px){
-   &:hover ${Meta}, &:hover ${Text}, &:hover ${SubTitle}{
-     display: block;
-     z-index: 100;
-   }
+  @media (min-width: 1200px) {
+    &:hover ${Meta}, &:hover ${Text}, &:hover ${SubTitle} {
+      display: block;
+      z-index: 100;
+    }
   }
 
-  &:first-of-type{
+  &:first-of-type {
     margin-left: 56px;
 
-    @media (max-width: 1000px){
+    @media (max-width: 1000px) {
       margin-left: 30px;
     }
-
   }
 
-  &:last-of-type{
+  &:last-of-type {
     margin-right: 56px;
 
-    @media(max-width: 1000px){
+    @media (max-width: 1000px) {
       margin-right: 30px;
     }
   }
-
-
 `;
 
 export const Image = styled.img`
