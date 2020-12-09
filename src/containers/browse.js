99a181fa90e6a2ca12a1aacd3d9f8103d8315b23
nelo <nelokwaz@gmail.com>
2020-12-09 13:34:18 +0100

    added the search functionality using fuse.js library

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
+import Fuse from "fuse.js";
 import SelectProfileContainer from "./profile";
 import FooterContainer from "./footer";
 import { FirebaseContext } from "../context/firebase";
@@ -27,6 +28,20 @@ export default function BrowseContainer({ slides }) {
   useEffect(() => {
     setSlideRows(slides[category]);
   }, [slides, category]);
+
+  useEffect(() => {
+    const fuse = new Fuse(slideRows, {
+      keys: ["data.description", "data.title", "data.genre"],
+    });
+
+    const results = fuse.search(searchTerm).map(({ item }) => item);
+
+    if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+      setSlideRows(results);
+    } else {
+      setSlideRows(slides[category]);
+    }
+  }, [searchTerm]);
 
   return profile.displayName ? (
     <>
