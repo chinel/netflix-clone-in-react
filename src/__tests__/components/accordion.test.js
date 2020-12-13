96fb9fea0f45c4cb7461c4ddb20df38b088b38ed
nelo <nelokwaz@gmail.com>
2020-12-13 08:44:37 +0100

    added the accordion layout structure to the accordion test file

--- src/__tests__/components/accordion.test.js
+++ src/__tests__/components/accordion.test.js
@@ -1,3 +1,20 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
+import faqsData from "../../fixtures/faqs.json";
 import { Accordion } from "../../components";
+
+describe("<Accordion />", () => {
+  it("renders the <Accordion /> with populated data", () => {
+    const { container, getByText } = render(
+      <Accordion>
+        <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+        {faqsData.map((item) => (
+          <Accordion.Item key={item.id}>
+            <Accordion.Header>{item.header}</Accordion.Header>
+            <Accordion.Body>{item.body}</Accordion.Body>
+          </Accordion.Item>
+        ))}
+      </Accordion>
+    );
+  });
+});
