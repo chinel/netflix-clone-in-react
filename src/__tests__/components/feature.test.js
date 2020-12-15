bf255c30698d1981dc0d38682c156d79efb5f132
nelo <nelokwaz@gmail.com>
2020-12-15 23:22:28 +0100

    added the Feature component layout structure

---
+++ src/__tests__/components/feature.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { Feature } from "../../components";
+
+describe("<Feature />", () => {
+  it("renders the <Feature /> with populated data", () => {
+    const { container, getByText } = render(
+      <Feature>
+        <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+        <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+      </Feature>
+    );
+  });
+});
