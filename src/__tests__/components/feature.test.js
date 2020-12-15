b56fc6aa6863042cbc42d8e2dd7ff3cfaea00702
nelo <nelokwaz@gmail.com>
2020-12-15 23:39:15 +0100

    added another test block to test just feature title

--- src/__tests__/components/feature.test.js
+++ src/__tests__/components/feature.test.js
@@ -27,4 +27,16 @@ describe("<Feature />", () => {
     expect(queryByText("Watch anywhere. Cancel at any time.")).toBeFalsy();
     expect(container.firstChild).toMatchSnapshot();
   });
+
+  it("renders the <Feature /> with just sub title", () => {
+    const { container, getByText, queryByText } = render(
+      <Feature>
+        <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+      </Feature>
+    );
+
+    expect(queryByText("Unlimited films, TV programmes and more.")).toBeFalsy();
+    expect(getByText("Watch anywhere. Cancel at any time.")).toBeTruthy();
+    expect(container.firstChild).toMatchSnapshot();
+  });
 });
