50f78cd47770e070780a64bd593eac31be634810
nelo <nelokwaz@gmail.com>
2020-12-17 16:38:13 +0100

    created the first test block for the opt component

--- src/__tests__/components/opt-form.test.js
+++ src/__tests__/components/opt-form.test.js
@@ -1,2 +1,19 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { OptForm } from "../../components";
+
+describe("<OptForm />", () => {
+  it("renders the <OptForm /> with populated data", () => {
+    const { container, getByText, getByPlaceholderText } = render(
+      <OptForm>
+        <OptForm.Input placeholder="Email Address" />
+        <OptForm.Button>Try it now</OptForm.Button>
+        <OptForm.Break />
+        <OptForm.Text>
+          Ready to watch? Enter your email address to create or restart your
+          membership.
+        </OptForm.Text>
+      </OptForm>
+    );
+  });
+});
