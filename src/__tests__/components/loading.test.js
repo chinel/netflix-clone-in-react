dee955cb42904316bd7de48b60661234e54ba718
nelo <nelokwaz@gmail.com>
2020-12-17 17:52:36 +0100

    added some assertions and made some corrections to the loading component

--- src/__tests__/components/loading.test.js
+++ src/__tests__/components/loading.test.js
@@ -7,5 +7,9 @@ describe("<Loading />", () => {
     const { container, getByTestId } = render(
       <Loading src="/images/karl.png" data-testid="loading" />
     );
+
+    expect(getByTestId("loading")).toBeTruthy();
+    expect(getByTestId("loading-picture")).toBeTruthy();
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -3,7 +3,7 @@ import { Spinner, LockBody, ReleaseBody, Picture } from "./styles/loading";
 
 export default function Loading({ src, ...restProps }) {
   return (
-    <Spinner>
+    <Spinner {...restProps}>
       <LockBody />
       <Picture src={`/images/users/${src}.png`} data-testid="loading-picture" />
     </Spinner>
