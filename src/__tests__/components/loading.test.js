e28ab524bdbcbd65f4206044bddf61c8b91af76c
nelo <nelokwaz@gmail.com>
2020-12-17 17:46:02 +0100

    added the loading component layout

--- src/__tests__/components/loading.test.js
+++ src/__tests__/components/loading.test.js
@@ -1,2 +1,11 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { Loading } from "../../components";
+
+describe("<Loading />", () => {
+  it("renders the <Loading /> component", () => {
+    const { container, getByTestId } = render(
+      <Loading src="/images/karl.png" data-testid="loading" />
+    );
+  });
+});
