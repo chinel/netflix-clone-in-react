a511b7d2f321e14bfa5b21c073751bdc24ee5d4d
nelo <nelokwaz@gmail.com>
2020-12-16 15:31:01 -1200

    cleaned up the form layout structure

--- src/__tests__/components/form.test.js
+++ src/__tests__/components/form.test.js
@@ -1,38 +1,34 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { Form } from "../../components";
+import * as ROUTES from "../../constants/routes";
 
 describe("<Form />", () => {
   it("renders the <Form /> with populated data", () => {
-    <Form>
-      <Form.Title>Sign In</Form.Title>
-      {error && <Form.Error>{error}</Form.Error>}
-      <Form.Base onSubmit={handleSignIn} method="POST">
-        <Form.Input
-          placeholder="Email Address"
-          value={emailAddress}
-          onChange={({ target }) => setEmailAddress(target.value)}
-        />
-        <Form.Input
-          placeholder="Password"
-          type="password"
-          autoComplete="off"
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
-        />
-        <Form.Submit disabled={isInvalid} type="submit">
-          Sign In
-        </Form.Submit>
-      </Form.Base>
+    const { container, getByText, getByPlaceholderText } = render(
+      <Form>
+        <Form.Title>Sign In</Form.Title>
+        <Form.Base>
+          <Form.Input placeholder="Email Address" onChange={() => {}} />
+          <Form.Input
+            placeholder="Password"
+            type="password"
+            onChange={() => {}}
+          />
+          <Form.Submit disabled type="submit">
+            Sign In
+          </Form.Submit>
+        </Form.Base>
 
-      <Form.Text>
-        New to Netflix?{" "}
-        <Form.TextLink to={ROUTES.SIGN_UP}>Sign up now.</Form.TextLink>
-      </Form.Text>
-      <Form.TextSmall>
-        This page is protected by Google reCaptcha to ensure that you're not a
-        bot. Learn more.
-      </Form.TextSmall>
-    </Form>;
+        <Form.Text>
+          New to Netflix?{" "}
+          <Form.TextLink to={ROUTES.SIGN_UP}>Sign up now.</Form.TextLink>
+        </Form.Text>
+        <Form.TextSmall>
+          This page is protected by Google reCaptcha to ensure that you're not a
+          bot. Learn more.
+        </Form.TextSmall>
+      </Form>
+    );
   });
 });
