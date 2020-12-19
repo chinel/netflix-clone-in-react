9f69f2b63e970c05c912c0adb266d0c6e9b52fbc
nelo <nelokwaz@gmail.com>
2020-12-18 18:21:02 -1200

    added th first test block for the jumbotron

--- src/__tests__/components/jumbotron.test.js
+++ src/__tests__/components/jumbotron.test.js
@@ -1,3 +1,10 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { Jumbotron } from "../../components";
+import JumboData from "../../fixtures/jumbo";
+
+describe("<Jumbotron/>", () => {
+  it("renders the <Jumbotron /> with populated data", () => {
+    const { container, getByText, getByAltText, getTestId } = render();
+  });
+});
