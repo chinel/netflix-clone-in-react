f142d2d92883ed6969b9aa953e2f8091627a08d1
nelo <nelokwaz@gmail.com>
2020-12-18 18:22:16 -1200

    added the jumbotron component layout to the test block

--- src/__tests__/components/jumbotron.test.js
+++ src/__tests__/components/jumbotron.test.js
@@ -5,6 +5,20 @@ import JumboData from "../../fixtures/jumbo";
 
 describe("<Jumbotron/>", () => {
   it("renders the <Jumbotron /> with populated data", () => {
-    const { container, getByText, getByAltText, getTestId } = render();
+    const { container, getByText, getByAltText, getTestId } = render(
+      <Jumbotron.Container>
+        {JumboData.map((data) => (
+          <Jumbotron key={data.id} direction={data.direction}>
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{data.title}</Jumbotron.Title>
+              <Jumbotron.SubTitle>{data.subTitle}</Jumbotron.SubTitle>
+            </Jumbotron.Pane>
+            <Jumbotron.Pane>
+              <Jumbotron.Image src={data.image} alt={data.alt} />
+            </Jumbotron.Pane>
+          </Jumbotron>
+        ))}
+      </Jumbotron.Container>
+    );
   });
 });
