841f2d403aeba6dd0dc8a360df4062577f4556e0
nelo <nelokwaz@gmail.com>
2020-12-18 20:45:53 -1200

    added some assertions to the test block

--- src/__tests__/components/jumbotron.test.js
+++ src/__tests__/components/jumbotron.test.js
@@ -5,20 +5,34 @@ import JumboData from "../../fixtures/jumbo";
 
 describe("<Jumbotron/>", () => {
   it("renders the <Jumbotron /> with populated data", () => {
-    const { container, getByText, getByAltText, getTestId } = render(
+    const { container, getByText, getByAltText, getByTestId } = render(
       <Jumbotron.Container>
         {JumboData.map((data) => (
-          <Jumbotron key={data.id} direction={data.direction}>
+          <Jumbotron key={data.id}>
             <Jumbotron.Pane>
               <Jumbotron.Title>{data.title}</Jumbotron.Title>
               <Jumbotron.SubTitle>{data.subTitle}</Jumbotron.SubTitle>
             </Jumbotron.Pane>
             <Jumbotron.Pane>
-              <Jumbotron.Image src={data.image} alt={data.alt} />
+              <Jumbotron.Image
+                src={data.image}
+                alt={data.alt}
+                data-testid={`${data.id}-jumbo-image`}
+              />
             </Jumbotron.Pane>
           </Jumbotron>
         ))}
       </Jumbotron.Container>
     );
+
+    expect(getByText("Enjoy on your TV.")).toBeTruthy();
+    expect(getByAltText("Tiger King on Netflix")).toBeTruthy();
+    expect(getByTestId("1-jumbo-image")).toBeTruthy();
+    expect(
+      getByText(
+        "Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more."
+      )
+    ).toBeTruthy();
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
