e2405dd3071fc1c609a81979361b00fc6edf265d
nelo <nelokwaz@gmail.com>
2020-12-19 11:51:21 +0100

    created the first test block for the header component

--- src/__tests__/components/header.test.js
+++ src/__tests__/components/header.test.js
@@ -1,3 +1,11 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import { Header } from "../../components";
+
+jest.mock("react-router-dom");
+
+describe("<Header/>", () => {
+  it("renders the <Header /> with populated data", () => {
+    const { container, getByText, getByTestId } = render();
+  });
+});
