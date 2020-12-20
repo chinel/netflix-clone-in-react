25803cc85b3860d22393ed7bed2a04b96b8dd470
nelo <nelokwaz@gmail.com>
2020-12-20 15:31:19 +0100

    added some dummy data for testing

--- src/__tests__/utils/selection-filter.test.js
+++ src/__tests__/utils/selection-filter.test.js
@@ -1,2 +1,20 @@
-import React from "react";
-import { render } from "@testing-library/react";
+import selectionFilter from "../../utils/selection-filter";
+
+test("selectionFilter with legitmate data", () => {
+  const series = [
+    {
+      title: "Documentaries",
+      data: [
+        {
+          id: "series-1x",
+          title: "Tiger King",
+          description:
+            "An exploration of big cat breeding and its bizarre underworld, populated by eccentric characters.",
+          genre: "documentaries",
+          maturity: "18",
+          slug: "tiger-king",
+        },
+      ],
+    },
+  ];
+});
